6327e9e9176a909cbc24bf5a604b28ed9dcb3b55
Stew47 <38304670+Stew47@users.noreply.github.com>
2018-04-19 11:36:37 -0400

    Update

--- Project2/src/Project2/CardGame.java
+++ Project2/src/Project2/CardGame.java
@@ -1,41 +1,36 @@
-/*Project 2
+/*CS 1302 
+ * Project 2
  * Edgar Perez & Tyler Stewart
- * 
 */
 package Project2;
 
-
-//game reset and counter/score --WIP
-//-score is not counting properly and reset is a little weird on running at the intended time, due in part to the possiblity of two or more transitions happening at once
-//score display for player
-//win condition thing
-//algorithm step the step
-//screenshots
-
 import javafx.animation.FadeTransition;
-import javafx.animation.Animation.Status;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.image.PixelReader;
 
 public class CardGame extends Application {
 
 	private int counter = 0;
 	private int score = 0;
+	private Label scoreBoard = new Label("Your score: 0");
+	private Label win = new Label();
 	private GridPane gpane = new GridPane();
 	private Image[] images = new Image[16];
-	//not working
+	//keeps track of last image clicked, with a default image that is never displayed/used elsewhere
 	private int lastCard = 16;
-	private boolean test = true;
+	private boolean animationRunning = false;
+	//keeps track which cards have been matched
+	private boolean[] matched = {false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false};
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -53,8 +48,14 @@ public class CardGame extends Application {
 		Button btQuit = new Button("Quit");
 		gpane.add(btQuit,0,5);
 		
+		//Label for score and win added to pane
+		gpane.add(scoreBoard, 3, 4);
+		gpane.add(win, 2, 4);
+		
+		
+		
 		//sets the title size and then shows the scene
-		Scene scene = new Scene(gpane,350,600);
+		Scene scene = new Scene(gpane,375,600);
 		primaryStage.setTitle("Memory Card Game");
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -71,7 +72,7 @@ public class CardGame extends Application {
 				new Image("image/num5.png"),new Image("image/num6.png"),new Image("image/num7.png"),new Image("image/num8.png")};				
 		int[] CardCount = {0,0,0,0,0,0,0,0};
 		
-		//random number var, used so that image usage can be counted in seperate array
+		//random number var, used so that image usage can be counted in separate array
 		int randnum;
 		
 		for (int i = 0; i < 16;i++) {
@@ -89,8 +90,8 @@ public class CardGame extends Application {
 	public void setPane() {
 		ImageView[] cards = new ImageView[17];
 		
-		cards[16] = new ImageView(new Image("image/num1.png"));
-		
+		//creates a default card which should never be seen by the user
+		cards[16] = new ImageView(new Image("image/blank.png"));
 		cards[16].setPreserveRatio(true);
 		cards[16].fitHeightProperty().bind(gpane.heightProperty().subtract(gpane.heightProperty().divide(5)).divide(4));
 		cards[16].fitWidthProperty().bind(gpane.widthProperty().subtract(20).divide(4));
@@ -103,85 +104,95 @@ public class CardGame extends Application {
 			image.fitHeightProperty().bind(gpane.heightProperty().subtract(gpane.heightProperty().divide(5)).divide(4));
 			image.fitWidthProperty().bind(gpane.widthProperty().subtract(20).divide(4));
 			
-			//sets the "back" to the array
+			//sets the card "back" to the array
 			cards[i] = image;
 			
+			//checks array to see if cards have already been matched, if so they are disabled and pre-flipped
+			cards[i].setDisable(matched[i]);
+			
+			if(cards[i].isDisable()) {
+				cards[i].setImage(images[i]);
+			}
+			
 			//two transitions, one to fade to transparent, one to fade back to original opacity
-			FadeTransition ft1 = new FadeTransition(Duration.millis(1000), image);
+			FadeTransition ft1 = new FadeTransition(Duration.millis(500), image);
 			ft1.setFromValue(1.0);
 			ft1.setToValue(0);
 			
-			FadeTransition ft2 = new FadeTransition(Duration.millis(1000), image);
+			FadeTransition ft2 = new FadeTransition(Duration.millis(600), image);
 			ft2.setToValue(1.0);
 			
-			//final so that cards event can actually use it, the value is reset on each loop
+			//final so that cards[i] event can actually use it, the value is set to each object and represents the position in the array
 			final int x = i;
 			
-			//here be dragons
-			//lots o' bugs/not fully/properly implemented things
 			//the bulk of the game logic involved in "flipping" the cards, playing animations, keeping score, etc.		
 			cards[i].setOnMousePressed(e -> {
-				//checks if the card is flipped, if it is not the animation is called and it is counted as one card flipped
-				//otherwise the code skips this
-				
-				if(compareImages(image.getImage(), new Image("image/front.png")) && test) { 
+				//checks if the card is flipped and a animation is already running, if it is not 
+				//the animation is called and it is counted as one card flipped otherwise the code skips this
+				if(compareImages(image.getImage(), new Image("image/front.png")) && !animationRunning) { 
 					counter += 1;
-					System.out.println("count" + counter);
-					test = false;
-					System.out.println("Running:" + test);
+					cards[x].setDisable(true);
+					
+					//set var to false, preventing additional clicks from running until animation is complete
+					animationRunning = true;
+					
 					ft1.play();
 				}
 				
 				//when the fade transition to blank is finished, the image is swapped with the "front" of the card and fades back to 
 				//normal opacity
 				ft1.setOnFinished(n -> {
-					
 					image.setImage(images[x]);
-					System.out.println("Card position: " +x);
 					
-					System.out.println("backs: " + compareImages(image.getImage(), cards[lastCard].getImage()));
 					if(compareImages(image.getImage(), cards[lastCard].getImage())) {
-						System.out.println("Same");
-						score += 1;
-						System.out.println(score);
+						//marks both cards as matched, preventing them from being clickable on next reset
+						matched[x] = true;
+						matched[lastCard] = true;
 						
+						score += 1;	
+						
+						//updates scoreboard
+						scoreBoard.setText("Your Score: " + score);
+					} else if(!compareImages(image.getImage(),cards[lastCard].getImage())) {
+						cards[x].setDisable(false);
 					}
 					
+					//set to current card, so that next run will know which card was clicked before it
 					lastCard = x;
 					ft2.play();
 				});
 				
-				 
-				 
 				//checks if the user score is equal to three, if so the game stops running and announces they have won
 				//otherwise if the user has clicked two cards the flipped counter is reset along with the pane, "flipping"
 				//the cards back to their "back". The cards however stay the same, as that is only set on first run
 				ft2.setOnFinished(n -> {			
-					
 					if(score == 3) {
-						System.out.println("You have won");
-					} else if (counter >= 2 && score != 3) {
+						win.setText("You win!");
+						
+						//calls a method, not required but fun
+						win();
+					} else if (counter == 2 && score != 3) {
 						counter = 0;
+						
+						//reset to default value
 						lastCard = 16;
+						
 						setPane();
-					}
-					test = true;
-					//TODO:Fix
-					//Should be used to keep last image clicked so that a comparision can be used, but currently bugged
-					//lastCard = images[x];
+					} 
+					//lets next click begin the animation
+					animationRunning = false;
 				});
 				
 			});
 		}
 		
 		//adds all the cards to the pane
-		int x = 0;
+		int k = 0;
 		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				gpane.add(cards[x], j, i);
-				x+=1;
-				
+				gpane.add(cards[k], j, i);
+				k+=1;				
 			}
 		}
 		
@@ -192,8 +203,7 @@ public class CardGame extends Application {
 		public boolean compareImages(Image pic1, Image pic2) {
 			boolean isSame = true;
 			
-			//compares the images first by pixel width/height, then compares the color of each pixel to the other,
-			//breaks out of the loop as soon as differences are found
+			//compares the images first by pixel width/height, then compares the color of each pixel to the other
 			if(pic1.getWidth()==pic2.getWidth()) {
 				if(pic1.getHeight()==pic2.getHeight()) {
 					for (int i = 0; pic1.getWidth() > i;i++) {
@@ -202,7 +212,6 @@ public class CardGame extends Application {
 									pic2.getPixelReader().getArgb(i, j)) {
 							} else {
 								isSame = false;
-								break;
 							}
 						}
 					}
@@ -219,8 +228,25 @@ public class CardGame extends Application {
 		launch(args);
 	}
 	
-	//TODO: Maybe throw a overloaded setPane() down here, called when game is won to show some victory things
-	//not at all needed, so feel free to ignore
+	//left at bottom as this is not required, just a fun thing for the win condition
+	public void win() {
+		gpane.getChildren().clear();
+		
+		Label text = new Label("You Win!");
+		text.setFont(new Font(30));
+		gpane.add(text, 1, 0);
+		
+		ImageView fireworks = new ImageView(new Image("image/fireworks.gif"));
+		fireworks.fitWidthProperty().bind(gpane.widthProperty());
+		fireworks.fitHeightProperty().bind(gpane.heightProperty().subtract(100));
+		gpane.add(fireworks, 1, 1);
+		
+		
+		
+		
+		
+		
+	}
 }
 
 
